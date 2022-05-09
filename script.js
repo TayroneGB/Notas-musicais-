4d5a69b983676dfb1464ecdfd4fb8d828b6e539b
Tayrone <tayrone.bez@gmail.com>
2022-05-09 13:49:38 -0300

    09/05

---
+++ script.js
@@ -0,0 +1,59 @@
+const note = new Audio();
+//Notas
+const baseDir = "./notas/";
+const natural = ["C", "D", "E", "F", "G", "A", "B"];
+const sharp = ["C#", "D#", "F#", "G#", "A#"];
+const full = [...natural, ...sharp];
+let selectedNotes = natural;
+let actualNote = null;
+//Seletores
+const iniciarTraining = document.querySelector(".iniciar");
+const blockNote = document.querySelector(".note");
+const showBlockNote = document.querySelector(".note .bg");
+const repetirNota = document.querySelector(".repetir");
+const showNote = document.querySelector(".show");
+const select = document.querySelector("select");
+const span = document.querySelector("span");
+const noteC = document.querySelector(".noteC");
+//Eventos
+select.addEventListener("change", handleChange);
+iniciarTraining.addEventListener("click", handleInit);
+
+repetirNota.addEventListener("click", () => {
+  note.src = actualNote;
+  note.play();
+});
+
+showNote.addEventListener("click", () => {
+  showBlockNote.classList.add("active");
+});
+noteC.addEventListener("click", handleNoteC);
+//Funções
+function handleNoteC() {
+  note.src = "./notas/CGrave.wav";
+  note.play();
+}
+function handleChange(event) {
+  if (event.target.value === "natural") {
+    selectedNotes = natural;
+  } else if (event.target.value === "sharp") {
+    selectedNotes = sharp;
+  } else {
+    selectedNotes = fullNotes;
+  }
+}
+function randomNotes(baseNotes) {
+  const actualNote = Math.floor(Math.random() * baseNotes.length);
+  return baseNotes[actualNote];
+}
+
+function handleInit() {
+  const randomNote = randomNotes(selectedNotes);
+  actualNote = `${baseDir}${encodeURIComponent(randomNote)}.wav`;
+  note.src = actualNote;
+  span.innerText = randomNote;
+  blockNote.setAttribute("data-note", randomNote);
+  showBlockNote.classList.remove("active");
+  note.play();
+  console.log(actualNote);
+}
